b754b7fc5d3f61f234e35c1da64a3d95f1d8a988
hippietilley <hippietilley@gmail.com>
2018-05-08 13:43:06 -0500

    Documented Log

--- utilities/src/main/java/org/md2k/utilities/AbstractActivityAccess.java
+++ utilities/src/main/java/org/md2k/utilities/AbstractActivityAccess.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.utilities;
 
 import android.app.Activity;
@@ -10,6 +37,9 @@ import org.md2k.utilities.permission.ObservablePermission;
 import rx.Observer;
 import rx.Subscription;
 
+/**
+ *
+ */
 public abstract class AbstractActivityAccess extends AppCompatActivity {
     public static final String SETTINGS = "settings";
     public static final String IS_VALID_SETTINGS = "is_valid_settings";
@@ -20,13 +50,12 @@ public abstract class AbstractActivityAccess extends AppCompatActivity {
     public static final String OPERATION = "operation";
     public static final String RESULT = "result";
     public static final String PERMISSION = "permission";
-    public static final int REQ_CODE=1;
+    public static final int REQ_CODE = 1;
     Subscription subsPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_abstract_access);
         String operation = getIntent().getStringExtra(OPERATION);
         performOperation(operation);
     }
@@ -35,7 +64,6 @@ public abstract class AbstractActivityAccess extends AppCompatActivity {
         subsPermission = ObservablePermission.get(this).subscribe(new Observer<Boolean>() {
             @Override
             public void onCompleted() {
-
             }
 
             @Override
@@ -63,7 +91,6 @@ public abstract class AbstractActivityAccess extends AppCompatActivity {
         resultIntent.putExtra(RESULT, result);
         setResult(Activity.RESULT_OK, resultIntent);
         finish();
-
     }
 
     public abstract boolean isValidSettings();
@@ -103,5 +130,4 @@ public abstract class AbstractActivityAccess extends AppCompatActivity {
                 break;
         }
     }
-
 }
