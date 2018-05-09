cbbf2db37d74597824064fd3e090a88feac57533
hippietilley <hippietilley@gmail.com>
2018-05-09 15:25:20 -0500

    Documented ResultType

--- utilities/src/main/java/org/md2k/utilities/data_format/ResultType.java
+++ utilities/src/main/java/org/md2k/utilities/data_format/ResultType.java
@@ -1,7 +1,6 @@
-package org.md2k.utilities.data_format;
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,19 +25,45 @@ package org.md2k.utilities.data_format;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package org.md2k.utilities.data_format;
+
+/**
+ * Wrapper class for <code>ActivityType</code>.
+ */
 public class ResultType {
+    /**
+     * Nested class for Activity type constants.
+     * <p>
+     *     Available constants are:
+     *  <ul>
+     *      <li><code>STILL</code> - Integer value = 0.</li>
+     *      <li><code>ON_FOOT</code> - Integer value = 1.</li>
+     *      <li><code>TILTING</code> - Integer value = 2.</li>
+     *      <li><code>WALKING</code> - Integer value = 3.</li>
+     *      <li><code>RUNNING</code> - Integer value = 4.</li>
+     *      <li><code>ON_BICYCLE</code> - Integer value = 5.</li>
+     *      <li><code>IN_VEHICLE</code> - Integer value = 6.</li>
+     *      <li><code>UNKNOWN</code> - Integer value = 7.</li>
+     *  </ul>
+     * </p>
+     */
     public class ActivityType{
-        public static final int STILL=0;
-        public static final int ON_FOOT=1;
-        public static final int TILTING=2;
-        public static final int WALKING=3;
-        public static final int RUNNING=4;
-        public static final int ON_BICYCLE=5;
-        public static final int IN_VEHICLE=6;
-        public static final int UNKNOWN=7;
+        public static final int STILL = 0;
+        public static final int ON_FOOT = 1;
+        public static final int TILTING = 2;
+        public static final int WALKING = 3;
+        public static final int RUNNING = 4;
+        public static final int ON_BICYCLE = 5;
+        public static final int IN_VEHICLE = 6;
+        public static final int UNKNOWN = 7;
         int value;
+
+        /**
+         * Constructor
+         * @param value Integer value for the desired activity type.
+         */
         ActivityType(int value){
-            this.value=value;
+            this.value = value;
         }
     }
 }
