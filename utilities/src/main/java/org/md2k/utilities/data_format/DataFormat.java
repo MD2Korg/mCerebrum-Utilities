8253a1af63ddcc46a7c6dac2ea3f0b1e09f2e518
hippietilley <hippietilley@gmail.com>
2018-05-09 15:10:59 -0500

    Documented DataFormat

--- utilities/src/main/java/org/md2k/utilities/data_format/DataFormat.java
+++ utilities/src/main/java/org/md2k/utilities/data_format/DataFormat.java
@@ -1,7 +1,6 @@
-package org.md2k.utilities.data_format;
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,44 +25,98 @@ package org.md2k.utilities.data_format;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package org.md2k.utilities.data_format;
+
+/**
+ * Provides integer constants for the data.
+ */
 public class DataFormat{
-    public static final int Pressure=0;
-    public static final int AmbientLight=0;
-    public static final int AmbientTemperature =0;
-    public static final int Proximity=0;
+    /** Integer constant for Pressure. <p>Default is 0.</p> */
+    public static final int Pressure = 0;
 
+    /** Integer constant for Ambient light. <p>Default is 0.</p> */
+    public static final int AmbientLight = 0;
+
+    /** Integer constant for Ambient temperature. <p>Default is 0.</p> */
+    public static final int AmbientTemperature = 0;
+
+    /** Integer constant for Proximity. <p>Default is 0.</p> */
+    public static final int Proximity = 0;
+
+    /**
+     * Nested class for Accelerometer constants.
+     */
     public class Accelerometer{
-        public static final int X=0;
-        public static final int Y=1;
-        public static final int Z=2;
+        /** Integer constant for the accelerometer's x axis. <p>Default is 0.</p> */
+        public static final int X = 0;
+        /** Integer constant for the accelerometer's y axis. <p>Default is 1.</p> */
+        public static final int Y = 1;
+        /** Integer constant for the accelerometer's z axis. <p>Default is 2.</p> */
+        public static final int Z = 2;
     }
+
+    /**
+     * Nested class for Gyroscope constants.
+     */
     public class Gyroscope{
-        public static final int X=0;
-        public static final int Y=1;
-        public static final int Z=2;
+        /** Integer constant for the gyroscope's x axis. <p>Default is 0.</p> */
+        public static final int X = 0;
+        /** Integer constant for the gyroscope's y axis. <p>Default is 1.</p> */
+        public static final int Y = 1;
+        /** Integer constant for the gyroscope's z axis. <p>Default is 2.</p> */
+        public static final int Z = 2;
     }
+
+    /**
+     * Nested class for Compass constants.
+     */
     public class Compass{
-        public static final int X=0;
-        public static final int Y=1;
-        public static final int Z=2;
+        /** Integer constant for the compass' x axis. <p>Default is 0.</p> */
+        public static final int X = 0;
+        /** Integer constant for the compass' y axis. <p>Default is 1.</p> */
+        public static final int Y = 1;
+        /** Integer constant for the compass' z axis. <p>Default is 2.</p> */
+        public static final int Z = 2;
     }
+
+    /**
+     * Nested class for ActivityType constants.
+     */
     public class ActivityType{
-        public static final int Type =0;
-        public static final int Confidence =1;
+        /** Integer constant for the activity Type. <p>Default is 0.</p> */
+        public static final int Type = 0;
+        /** Integer constant for the Confidence. <p>Default is 1.</p> */
+        public static final int Confidence = 1;
     }
 
+    /**
+     * Nested class for Battery constants.
+     */
     public class Battery {
-        public static final int Parcentage=0;
-        public static final int Voltage=1;
-        public static final int Temperature=2;
+        /** Integer constant for the battery Percentage. <p>Default is 0.</p> */
+        // TODO: Fix spelling on percentage.
+        public static final int Parcentage = 0;
+        /** Integer constant for the battery Voltage. <p>Default is 1.</p> */
+        public static final int Voltage = 1;
+        /** Integer constant for the battery Temperature. <p>Default is 2.</p> */
+        public static final int Temperature = 2;
     }
 
+    /**
+     * Nested class for Location constants.
+     */
     public class Location {
-        public static final int Latitude=0;
-        public static final int Longitude=1;
-        public static final int Altitude=2;
-        public static final int Speed=3;
-        public static final int Bearing=4;
-        public static final int Accuracy=5;
+        /** Integer constant for the Latitude. <p>Default is 0.</p> */
+        public static final int Latitude = 0;
+        /** Integer constant for the Longitude. <p>Default is 1.</p> */
+        public static final int Longitude = 1;
+        /** Integer constant for the Altitude. <p>Default is 2.</p> */
+        public static final int Altitude = 2;
+        /** Integer constant for the Speed. <p>Default is 3.</p> */
+        public static final int Speed = 3;
+        /** Integer constant for the Bearing. <p>Default is 4.</p> */
+        public static final int Bearing = 4;
+        /** Integer constant for the Accuracy. <p>Default is 5.</p> */
+        public static final int Accuracy = 5;
     }
 }
